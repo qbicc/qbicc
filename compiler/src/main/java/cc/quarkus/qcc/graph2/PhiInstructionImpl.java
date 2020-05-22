8bda730a1511b9fc1015ae452a53e6cb776559a8
David M. Lloyd <david.lloyd@redhat.com>
2020-05-22 13:46:21 -0500

    Show edge from phi instruction to phi value

--- compiler/src/main/java/cc/quarkus/qcc/graph2/PhiInstructionImpl.java
+++ compiler/src/main/java/cc/quarkus/qcc/graph2/PhiInstructionImpl.java
@@ -1,5 +1,8 @@
 package cc.quarkus.qcc.graph2;
 
+import java.io.IOException;
+import java.util.Set;
+
 final class PhiInstructionImpl extends InstructionImpl implements PhiInstruction {
     private final PhiValue phiValue;
 
@@ -13,5 +16,11 @@ final class PhiInstructionImpl extends InstructionImpl implements PhiInstruction
 
     public String getLabelForGraph() {
         return "inst:phi";
+    }
+
+    public Appendable writeToGraph(final Set<Node> visited, final Appendable graph, final Set<BasicBlock> knownBlocks) throws IOException {
+        super.writeToGraph(visited, graph, knownBlocks);
+        addEdgeTo(visited, graph, phiValue, "value", "yellow", "solid", knownBlocks);
+        return graph;
     }
 }
