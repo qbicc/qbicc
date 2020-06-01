22371dd940782bd9d7ab459cae3d328add1f4093
Bob McWhirter <bmcwhirt@redhat.com>
2020-06-01 16:32:21 -0400

    Remove commented-out.

--- compiler/src/main/java/cc/quarkus/qcc/constraint/Constraint.java
+++ compiler/src/main/java/cc/quarkus/qcc/constraint/Constraint.java
@@ -51,22 +51,5 @@ public interface Constraint {
         return isSatisfiedBy( new SatisfactionContextImpl( value ) );
     }
 
-    /** Determine if this constraint satisfies another constraint.
-     *
-     * This constraint may be stronger than the other context, in which case,
-     * the answer will be YES. If this constraint is irrelevant to the other
-     * constraint, the answer will be NOT_APPLICABLE. It will only be a definite
-     * NO in the case of recursive unsolvable constraint relations.
-     *
-     * You probably want to use {@link #isSatisfiedBy}. This method should
-     * probably be moved into the Impl's and change some casting/signatures
-     * within the others.
-     *
-     * @param context The target and other book-keeping context.
-     * @param other The other constraint to test.
-     * @return YES if this constraint satisfies, the other, else NOT_APPLICABLE,
-     * or maybe NO.
-     */
-    //Satisfaction satisfies(SatisfactionContext context, RelationConstraint other);
 }
 
