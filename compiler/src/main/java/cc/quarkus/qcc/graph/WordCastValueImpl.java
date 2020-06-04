af3346e40f537e2855773790b82833b8410edb76
David M. Lloyd <david.lloyd@redhat.com>
2020-06-04 14:07:56 -0500

    Create word cast value, but do not wire it in yet

---
+++ compiler/src/main/java/cc/quarkus/qcc/graph/WordCastValueImpl.java
@@ -0,0 +1,16 @@
+package cc.quarkus.qcc.graph;
+
+import io.smallrye.common.constraint.Assert;
+
+final class WordCastValueImpl extends CastValueImpl implements WordCastValue {
+    Kind kind;
+
+    public Kind getKind() {
+        return kind;
+    }
+
+    public void setKind(final Kind kind) {
+        Assert.checkNotNullParam("kind", kind);
+        this.kind = kind;
+    }
+}
