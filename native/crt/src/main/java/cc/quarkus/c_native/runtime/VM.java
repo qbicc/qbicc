5c2dfabe6f24e3bdff7e8098084458ad5e8d09a3
David M. Lloyd <david.lloyd@redhat.com>
2020-09-22 11:03:02 -0500

    Minor cleanups

--- native/crt/src/main/java/cc/quarkus/c_native/runtime/VM.java
+++ native/crt/src/main/java/cc/quarkus/c_native/runtime/VM.java
@@ -1,25 +1,8 @@
 package cc.quarkus.c_native.runtime;
 
-import static cc.quarkus.c_native.api.CNative.*;
-
-import cc.quarkus.api.ThreadScoped;
-
 /**
- * The overall VM state.
+ * VM Utilities.
  */
 public final class VM {
 
-
-    @export
-    @ThreadScoped
-    static ptr<?> ni_current_thread;
-
-
-
-    /**
-     * The VM thread state.
-     */
-    public static final class Thread {
-
-    }
 }
