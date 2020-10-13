3318c93aec29106fe57ca869f6bc11722f3cac9d
David M. Lloyd <david.lloyd@redhat.com>
2020-10-13 10:09:08 -0500

    Actions are Nodes

--- compiler/graph/src/main/java/cc/quarkus/qcc/graph/Action.java
+++ compiler/graph/src/main/java/cc/quarkus/qcc/graph/Action.java
@@ -3,6 +3,6 @@ package cc.quarkus.qcc.graph;
 /**
  * A node which represents an action whose side-effects are not captured as a value.
  */
-public interface Action {
+public interface Action extends Node {
     <T, R> R accept(ActionVisitor<T, R> visitor, T param);
 }
