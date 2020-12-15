b2b374565c791258c5de89590b6e4cedcc16d2da
David M. Lloyd <david.lloyd@redhat.com>
2020-12-15 13:39:51 -0600

    Convert a null throw to an NPE

--- plugins/try-catch/src/main/java/cc/quarkus/qcc/plugin/trycatch/LocalThrowHandlingBasicBlockBuilder.java
+++ plugins/try-catch/src/main/java/cc/quarkus/qcc/plugin/trycatch/LocalThrowHandlingBasicBlockBuilder.java
@@ -1,5 +1,7 @@
 package cc.quarkus.qcc.plugin.trycatch;
 
+import java.util.List;
+
 import cc.quarkus.qcc.context.CompilationContext;
 import cc.quarkus.qcc.graph.BasicBlock;
 import cc.quarkus.qcc.graph.BasicBlockBuilder;
@@ -9,7 +11,11 @@ import cc.quarkus.qcc.graph.Try;
 import cc.quarkus.qcc.graph.Value;
 import cc.quarkus.qcc.graph.literal.ClassTypeIdLiteral;
 import cc.quarkus.qcc.graph.literal.TypeIdLiteral;
+import cc.quarkus.qcc.type.NullType;
 import cc.quarkus.qcc.type.ReferenceType;
+import cc.quarkus.qcc.type.definition.ClassContext;
+import cc.quarkus.qcc.type.definition.ValidatedTypeDefinition;
+import cc.quarkus.qcc.type.descriptor.MethodDescriptor;
 
 /**
  * The basic block builder which handles any "local" {@code throw} to a handler in the same method or function.
@@ -20,6 +26,13 @@ public class LocalThrowHandlingBasicBlockBuilder extends DelegatingBasicBlockBui
     }
 
     public BasicBlock throw_(final Value value) {
+        if (value.getType() instanceof NullType) {
+            ClassContext classContext = getCurrentElement().getEnclosingType().getContext();
+            ValidatedTypeDefinition npe = classContext.findDefinedType("java/lang/NullPointerException").validate();
+            Value ex = new_((ClassTypeIdLiteral) npe.getTypeId());
+            ex = invokeConstructor(ex, npe.resolveConstructorElement(MethodDescriptor.VOID_METHOD_DESCRIPTOR), List.of());
+            return throw_(ex);
+        }
         ReferenceType valueType = (ReferenceType) value.getType();
         Try.CatchMapper catchMapper = getCatchMapper();
         TypeIdLiteral upperBound = valueType.getUpperBound();
