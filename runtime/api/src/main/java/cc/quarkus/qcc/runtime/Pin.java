966cdb700cb90d452f42de015fed59fe53edc59b
David M. Lloyd <david.lloyd@redhat.com>
2021-01-04 11:19:55 -0600

    Fast and slow pins

--- runtime/api/src/main/java/cc/quarkus/qcc/runtime/Pin.java
+++ runtime/api/src/main/java/cc/quarkus/qcc/runtime/Pin.java
@@ -12,7 +12,26 @@ public final class Pin<T> extends StackObject implements AutoCloseable {
         this.obj = obj;
     }
 
-    public static native <T> Pin<T> create(T obj);
+    /**
+     * Create a "fast" pin.  Such pins should be truly "fast" to avoid garbage collection delays and related problems.
+     * The implementation may, for example, suspend all copying collection for the duration of the pin.
+     *
+     * @param obj the object to pin
+     * @param <T> the object type
+     * @return the pin of the object which <em>must</em> be subsequently released
+     */
+    public static native <T> Pin<T> fast(T obj);
+
+    /**
+     * Create a "slow" pin.  Such pins may be retained for arbitrary amounts of time, but <em>should</em> be
+     * eventually released in most cases to avoid memory leaks.  The implementation may, for example, cause the
+     * pinned object to be promoted to non-copying heap generation.
+     *
+     * @param obj the object to pin
+     * @param <T> the object type
+     * @return the pin of the object which <em>must</em> be subsequently released
+     */
+    public static native <T> Pin<T> slow(T obj);
 
     public T getPinnedObject() {
         return obj;
