7593966de1a311c32b3e14dbb6d64d4d91ebafe4
David M. Lloyd <david.lloyd@redhat.com>
2021-01-08 11:24:27 -0600

    Add memcpy

--- runtime/api/src/main/java/cc/quarkus/qcc/runtime/stdc/String.java
+++ runtime/api/src/main/java/cc/quarkus/qcc/runtime/stdc/String.java
@@ -11,4 +11,6 @@ public class String {
     public static native size_t strlen(ptr<@c_const c_char> s);
 
     public static native ptr<c_char> strerror(c_int errNum);
+
+    public static native ptr<?> memcpy(ptr<?> dest, ptr<@c_const ?> src, size_t n);
 }
