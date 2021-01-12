43d9766344185334cc70011704bd0d03dd6c85a9
Foivos Zakkak <fzakkak@redhat.com>
2021-01-12 16:20:36 +0200

    More generic handling of shift masking

--- plugins/correctness/src/main/java/cc/quarkus/qcc/plugin/correctness/ShiftDistanceMaskingBasicBlockBuilder.java
+++ plugins/correctness/src/main/java/cc/quarkus/qcc/plugin/correctness/ShiftDistanceMaskingBasicBlockBuilder.java
@@ -1,11 +1,14 @@
 package cc.quarkus.qcc.plugin.correctness;
 
 import cc.quarkus.qcc.context.CompilationContext;
-import cc.quarkus.qcc.graph.*;
+import cc.quarkus.qcc.graph.BasicBlockBuilder;
+import cc.quarkus.qcc.graph.DelegatingBasicBlockBuilder;
+import cc.quarkus.qcc.graph.Value;
 import cc.quarkus.qcc.graph.literal.LiteralFactory;
+import cc.quarkus.qcc.type.IntegerType;
 import cc.quarkus.qcc.type.SignedIntegerType;
+import cc.quarkus.qcc.type.UnsignedIntegerType;
 import cc.quarkus.qcc.type.ValueType;
-import io.smallrye.common.constraint.Assert;
 
 /**
  * This builder masks shift distances to ensure they match the word width of the target architecture.
@@ -20,28 +23,39 @@ public class ShiftDistanceMaskingBasicBlockBuilder extends DelegatingBasicBlockB
 
     @Override
     public Value shl(Value v1, Value v2) {
-        return super.shl(v1, getMaskedShiftDistance(v1, v2));
+        return super.shl(v1, castAndMaskShiftDistance(v1, v2));
     }
 
     @Override
     public Value shr(Value v1, Value v2) {
-        return super.shr(v1, getMaskedShiftDistance(v1, v2));
+        return super.shr(v1, castAndMaskShiftDistance(v1, v2));
     }
 
-    private Value getMaskedShiftDistance(Value v1, Value v2) {
-        final SignedIntegerType signedInteger32Type = ctxt.getTypeSystem().getSignedInteger32Type();
-        final SignedIntegerType signedInteger64Type = ctxt.getTypeSystem().getSignedInteger64Type();
+    private Value castAndMaskShiftDistance(Value op, Value shiftDistance) {
+        final ValueType opType = op.getType();
 
-        final ValueType v1Type = v1.getType();
-        Assert.assertTrue(v1Type == signedInteger32Type || v1Type == signedInteger64Type);
-
-        final LiteralFactory lf = ctxt.getLiteralFactory();
-        final long bits = v1.getType().getSize() * ctxt.getTypeSystem().getByteBits();
-        Value v2Masked = and(v2, lf.literalOf(bits - 1));
-        if (v1Type == signedInteger64Type) {
-            v2Masked = extend(v2, signedInteger64Type);
+        // Correct shiftDistance's signdness to match that of op
+        ValueType shiftType = shiftDistance.getType();
+        if (opType instanceof SignedIntegerType && shiftType instanceof UnsignedIntegerType) {
+            shiftDistance = bitCast(shiftDistance, ((UnsignedIntegerType) shiftType).asSigned());
+        } else if (opType instanceof UnsignedIntegerType && shiftType instanceof SignedIntegerType) {
+            shiftDistance = bitCast(shiftDistance, ((SignedIntegerType) shiftType).asUnsigned());
         }
-        return v2Masked;
+
+        // Correct shiftDistance's size to match that of op
+        shiftType = shiftDistance.getType();
+        if (opType.getSize() < shiftType.getSize()) {
+            shiftDistance = truncate(shiftDistance, (IntegerType) opType);
+        } else if (opType.getSize() > shiftType.getSize()) {
+            shiftDistance = extend(shiftDistance, (IntegerType) opType);
+        }
+
+        // Mask the shift distance
+        final LiteralFactory lf = ctxt.getLiteralFactory();
+        final long bits = op.getType().getSize() * ctxt.getTypeSystem().getByteBits();
+        shiftDistance = and(shiftDistance, lf.literalOf(bits - 1));
+
+        return shiftDistance;
     }
 
 }
