37ed93b6a9ce7b61ddecfecc9004d95ceed2f1d6
Ashutosh Mehra <asmehra@redhat.com>
2021-01-27 14:44:27 -0500

    Convert NPE to compilation error when not able to find Unwind class
    If the jar file hosting Unwind class is not specified on boot classpath,
    then the compilation should fail with a proper exception message.
    Also updated the example to include required jar files on the boot
    classpath.
    
    Signed-off-by: Ashutosh Mehra <asmehra@redhat.com>

--- examples/helloworld/hello/world/Main.java
+++ examples/helloworld/hello/world/Main.java
@@ -7,7 +7,7 @@
 // $ jar cvf main.jar hello/world/Main.class
 //
 // Build the native executable in /tmp/output with:
-// $ jbang cc.quarkus:qcc-main:1.0.0-SNAPSHOT --boot-module-path ~/.m2/repository/cc/quarkus/qccrt-java.base/11.0.1-SNAPSHOT/qccrt-java.base-11.0.1-SNAPSHOT.jar:main.jar --output-path /tmp/output hello.world.Main
+// $ jbang cc.quarkus:qcc-main:1.0.0-SNAPSHOT --boot-module-path $HOME/.m2/repository/cc/quarkus/qccrt-java.base/11.0.1-SNAPSHOT/qccrt-java.base-11.0.1-SNAPSHOT.jar:$HOME/.m2/repository/cc/quarkus/qcc-runtime-api/1.0.0-SNAPSHOT/qcc-runtime-api-1.0.0-SNAPSHOT.jar:$HOME/.m2/repository/cc/quarkus/qcc-runtime-unwind/1.0.0-SNAPSHOT/qcc-runtime-unwind-1.0.0-SNAPSHOT.jar:main.jar --output-path /tmp/output hello.world.Main
 //
 // Run the executable
 // $ /tmp/output/a.out
