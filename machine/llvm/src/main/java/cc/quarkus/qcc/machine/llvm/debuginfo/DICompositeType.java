5cdb44a97c5230c15a84f52a406cbd12e38cde82
Benjamin <ben@benthomas.ca>
2021-02-10 10:10:26 -0500

    Add a baseType to DICompositeType
    Previously, there was no way to specify the baseType attribute of a
    DICompositeType. While this attribute is not used for structs, it is
    used for arrays to declare the element type. Support for setting this
    attribute has been added to allow DICompositeTypes to be created for
    arrays.

--- machine/llvm/src/main/java/cc/quarkus/qcc/machine/llvm/debuginfo/DICompositeType.java
+++ machine/llvm/src/main/java/cc/quarkus/qcc/machine/llvm/debuginfo/DICompositeType.java
@@ -6,6 +6,7 @@ import java.util.EnumSet;
 
 public interface DICompositeType extends MetadataNode {
     DICompositeType elements(LLValue elements);
+    DICompositeType baseType(LLValue baseType);
     DICompositeType name(String name);
     DICompositeType flags(EnumSet<DIFlags> flags);
     DICompositeType location(LLValue file, int line);
