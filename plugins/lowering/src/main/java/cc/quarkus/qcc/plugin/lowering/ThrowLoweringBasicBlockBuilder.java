067e23af5b1fd2ae2d50dd15ff67e7b10441bbff
Galder Zamarreño <galder@redhat.com>
2021-02-17 14:19:53 +0100

    Add missing declare for _Unwind_RaiseException

--- plugins/lowering/src/main/java/cc/quarkus/qcc/plugin/lowering/ThrowLoweringBasicBlockBuilder.java
+++ plugins/lowering/src/main/java/cc/quarkus/qcc/plugin/lowering/ThrowLoweringBasicBlockBuilder.java
@@ -7,6 +7,7 @@ import cc.quarkus.qcc.graph.BasicBlock;
 import cc.quarkus.qcc.graph.BasicBlockBuilder;
 import cc.quarkus.qcc.graph.DelegatingBasicBlockBuilder;
 import cc.quarkus.qcc.graph.Value;
+import cc.quarkus.qcc.type.FunctionType;
 
 public class ThrowLoweringBasicBlockBuilder extends DelegatingBasicBlockBuilder {
     private final CompilationContext ctxt;
@@ -19,7 +20,10 @@ public class ThrowLoweringBasicBlockBuilder extends DelegatingBasicBlockBuilder 
     public BasicBlock throw_(final Value value) {
         ThrowExceptionHelper teh = ThrowExceptionHelper.get(ctxt);
         Value ptr = addressOf(instanceFieldOf(referenceHandle(currentThread()), teh.getUnwindExceptionField()));
-        callFunction(ctxt.getLiteralFactory().literalOfSymbol("_Unwind_RaiseException", teh.getRaiseExceptionMethod().getType(List.of(/**/))), List.of(ptr));
+        String functionName = "_Unwind_RaiseException";
+        FunctionType functionType = teh.getRaiseExceptionMethod().getType(List.of(/**/));
+        ctxt.getImplicitSection(getCurrentElement()).declareFunction(teh.getRaiseExceptionMethod(), functionName, functionType);
+        callFunction(ctxt.getLiteralFactory().literalOfSymbol(functionName, functionType), List.of(ptr));
         return unreachable();
     }
 }
