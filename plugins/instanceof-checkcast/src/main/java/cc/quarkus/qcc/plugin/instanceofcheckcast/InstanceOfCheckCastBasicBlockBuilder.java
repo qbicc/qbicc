151e97c9d53e1cc2f377d00ac26ae81d735e909a
Dan Heidinga <heidinga@redhat.com>
2021-02-23 15:57:41 -0500

    Working (ugly) instanceof of classes
    This has a working version of instanceof for class types, including
    the case where the input may be null at runtime.
    
    Code needs cleanup and some reformatting as well as looking at the
    order of the blocks to avoid the backward branch in the notNull
    case.
    
    Signed-off-by: Dan Heidinga <heidinga@redhat.com>

--- plugins/instanceof-checkcast/src/main/java/cc/quarkus/qcc/plugin/instanceofcheckcast/InstanceOfCheckCastBasicBlockBuilder.java
+++ plugins/instanceof-checkcast/src/main/java/cc/quarkus/qcc/plugin/instanceofcheckcast/InstanceOfCheckCastBasicBlockBuilder.java
@@ -2,18 +2,30 @@ package cc.quarkus.qcc.plugin.instanceofcheckcast;
 
 import java.util.List;
 
+import org.jboss.logging.Logger;
+
 import cc.quarkus.qcc.context.CompilationContext;
 import cc.quarkus.qcc.graph.BasicBlockBuilder;
+import cc.quarkus.qcc.graph.BlockLabel;
 import cc.quarkus.qcc.graph.DelegatingBasicBlockBuilder;
+import cc.quarkus.qcc.graph.PhiValue;
+import cc.quarkus.qcc.graph.literal.IntegerLiteral;
+import cc.quarkus.qcc.graph.literal.Literal;
+import cc.quarkus.qcc.graph.literal.LiteralFactory;
+import cc.quarkus.qcc.graph.literal.NullLiteral;
 import cc.quarkus.qcc.graph.literal.LiteralFactory;
 import cc.quarkus.qcc.graph.Value;
 import cc.quarkus.qcc.object.Function;
 import cc.quarkus.qcc.type.definition.ClassContext;
 import cc.quarkus.qcc.type.definition.DefinedTypeDefinition;
 import cc.quarkus.qcc.type.definition.element.MethodElement;
+import cc.quarkus.qcc.type.descriptor.ArrayTypeDescriptor;
+import cc.quarkus.qcc.type.descriptor.TypeDescriptor;
 import cc.quarkus.qcc.type.definition.ValidatedTypeDefinition;
+import cc.quarkus.qcc.type.ClassObjectType;
 import cc.quarkus.qcc.type.NullType;
 import cc.quarkus.qcc.type.ReferenceType;
+import cc.quarkus.qcc.type.TypeType;
 import cc.quarkus.qcc.type.ValueType;
 
 /**
@@ -21,9 +33,12 @@ import cc.quarkus.qcc.type.ValueType;
  * RuntimeHelper APIs.
  */
 public class InstanceOfCheckCastBasicBlockBuilder extends DelegatingBasicBlockBuilder {
+    private static final Logger log = Logger.getLogger("cc.quarkus.qcc.plugin.instanceofcheckcast");
+    private static final Logger supersLog = Logger.getLogger("cc.quarkus.qcc.plugin.instanceofcheckcast.supers");
+    
     private final CompilationContext ctxt;
 
-    static final boolean PLUGIN_DISABLED = true;
+    static final boolean PLUGIN_DISABLED = true; // don't commit yet
 
     public InstanceOfCheckCastBasicBlockBuilder(final CompilationContext ctxt, final BasicBlockBuilder delegate) {
         super(delegate);
@@ -45,12 +60,13 @@ public class InstanceOfCheckCastBasicBlockBuilder extends DelegatingBasicBlockBu
     }
 
     public Value instanceOf(final Value input, final ValueType expectedType) {
+        LiteralFactory lf = ctxt.getLiteralFactory();
         // "null" instanceof <X> is always false
         if (NullType.isAlwaysNull(input)) {
-            return ctxt.getLiteralFactory().literalOf(false);
+            return lf.literalOf(false);
         }
+        
         // statically true instanceof checks are equal to x != null
-        LiteralFactory lf = ctxt.getLiteralFactory();
         if (expectedType instanceof ReferenceType) {
             ReferenceType refExpectedType = (ReferenceType) expectedType;
             ValueType actualType = input.getType();
@@ -61,6 +77,74 @@ public class InstanceOfCheckCastBasicBlockBuilder extends DelegatingBasicBlockBu
                 }
             }
         }
+
+        // TODO: missing null check on the input
+        final BlockLabel nullPath = new BlockLabel();
+        final BlockLabel notNull = new BlockLabel();
+        final BlockLabel after = new BlockLabel();
+        final NullLiteral nullLiteral = lf.literalOfNull();
+        //PhiValue phi = phi(ctxt.getTypeSystem().getBooleanType(), after);
+        if_(isEq(input, nullLiteral), nullPath, notNull);
+        begin(nullPath);
+        //PhiValue phi = phi(ctxt.getTypeSystem().getBooleanType(), after);
+        // (final CompilationContext ctxt, final Element element, final BlockLabel input, final Value value)
+        //phi.setValueForBlock(ctxt, getCurrentElement(), nullPath, lf.literalOf(false));
+        goto_(after);
+        begin(notNull);
+
+        // 3 cases:
+        // 1 - expectedType statically known to be a class
+        // 2 - expectedType statically known to be an interface
+        // 3 - expectedType statically known to be an array class
+Value result = null;
+        if (expectedType instanceof ReferenceType) {
+            ReferenceType refExpectedType = (ReferenceType) expectedType;
+            ClassObjectType cotExpectedType = (ClassObjectType)refExpectedType.getUpperBound();
+            ValidatedTypeDefinition vtdExpectedType = cotExpectedType.getDefinition().validate();
+            TypeDescriptor descriptor = vtdExpectedType.getDescriptor();
+            if (descriptor instanceof ArrayTypeDescriptor) {
+                // an array
+            } else {
+                if (vtdExpectedType.isInterface()) {
+                    // interface
+                } else {
+                    // class
+                    // Two cases here: leaf classes and those with subclasses
+                    Value inputTypeId = typeIdOf(referenceHandle(input));
+                    final int typeId = vtdExpectedType.getTypeId();
+                    final int maxSubId = vtdExpectedType.getMaximumSubtypeId();
+                    Literal vtdTypeId = ctxt.getLiteralFactory().literalOf(typeId);
+                    if (typeId == maxSubId) {
+                        supersLog.debug("instanceof: leaf class: direct compare: (" 
+                        + typeId
+                        + ") " + vtdExpectedType.getInternalName());
+                        // "leaf" class case - use direct comparison
+                        //return super.isEq(inputTypeId, vtdTypeId);
+                        //phi.setValueForBlock(ctxt, getCurrentElement(), notNull, super.isEq(inputTypeId, vtdTypeId));
+                        result = super.isEq(inputTypeId, vtdTypeId);
+                    } else {
+                        // "non-leaf" class case
+                        // (instanceId - castClassId <= (castClassId.range - castClassId)
+                        IntegerLiteral allowedRange = ctxt.getLiteralFactory().literalOf(maxSubId - typeId);
+                        supersLog.debug("instanceof: non-leaf class: allowedRange: (" 
+                            + maxSubId + " - " + typeId + ") = " + allowedRange.longValue()
+                            + " " + vtdExpectedType.getInternalName());
+                        
+                        Value subtract = sub(inputTypeId, vtdTypeId);
+                        //return super.isLe(subtract, allowedRange);
+                        //phi.setValueForBlock(ctxt, getCurrentElement(), notNull, super.isLe(subtract, allowedRange));
+                        result = super.isLe(subtract, allowedRange);
+                    }
+                }
+            }
+            goto_(after);
+            begin(after);
+            PhiValue phi = phi(ctxt.getTypeSystem().getBooleanType(), after);
+            phi.setValueForBlock(ctxt, getCurrentElement(), nullPath, lf.literalOf(false));
+            phi.setValueForBlock(ctxt, getCurrentElement(), notNull, result);
+            return phi;
+        }
+
 
         if (PLUGIN_DISABLED) {
             return super.instanceOf(input, expectedType);
