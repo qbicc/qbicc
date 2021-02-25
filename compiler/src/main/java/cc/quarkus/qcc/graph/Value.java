8458490350433bc5b0ab34f03de24f4d936f05c9
David M. Lloyd <david.lloyd@redhat.com>
2021-02-25 12:09:50 -0600

    Initial simple definite-conditions support

--- compiler/src/main/java/cc/quarkus/qcc/graph/Value.java
+++ compiler/src/main/java/cc/quarkus/qcc/graph/Value.java
@@ -11,4 +11,36 @@ public interface Value extends Node {
     // static
 
     Value[] NO_VALUES = new Value[0];
+
+    default boolean isDefEq(Value other) {
+        return equals(other);
+    }
+
+    default boolean isDefNe(Value other) {
+        return false;
+    }
+
+    default boolean isDefLt(Value other) {
+        return false;
+    }
+
+    default boolean isDefGt(Value other) {
+        return false;
+    }
+
+    default boolean isDefLe(Value other) {
+        return equals(other);
+    }
+
+    default boolean isDefGe(Value other) {
+        return equals(other);
+    }
+
+    default boolean isDefNaN() {
+        return false;
+    }
+
+    default boolean isDefNotNaN() {
+        return false;
+    }
 }
