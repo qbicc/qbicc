6262157e03c694705c5965ebd6b6a1ec85b9d4a2
David Grove <groved@us.ibm.com>
2021-05-11 10:25:39 -0400

    force NoGcHelpers clinit to be reachable

--- plugins/gc/nogc/src/main/java/org/qbicc/plugin/gc/nogc/NoGcSetupHook.java
+++ plugins/gc/nogc/src/main/java/org/qbicc/plugin/gc/nogc/NoGcSetupHook.java
@@ -3,6 +3,7 @@ package org.qbicc.plugin.gc.nogc;
 import java.util.function.Consumer;
 
 import org.qbicc.context.CompilationContext;
+import org.qbicc.type.definition.element.InitializerElement;
 
 /**
  *
@@ -11,6 +12,12 @@ public class NoGcSetupHook implements Consumer<CompilationContext> {
     public void accept(final CompilationContext ctxt) {
         ctxt.registerEntryPoint(NoGc.get(ctxt).getAllocateMethod());
         ctxt.registerEntryPoint(NoGc.get(ctxt).getCopyMethod());
-        ctxt.registerEntryPoint((NoGc.get(ctxt).getZeroMethod()));
+        ctxt.registerEntryPoint(NoGc.get(ctxt).getZeroMethod());
+
+        // Annoying since this clinit should be empty, but we need to make the clinit an entrypoint if we are making its static methods entrypoints.
+        InitializerElement clinit = ctxt.getBootstrapClassContext().findDefinedType("org/qbicc/runtime/gc/nogc/NoGcHelpers").load().getInitializer();
+        if (clinit != null) {
+            ctxt.registerEntryPoint(clinit);
+        }
     }
 }
