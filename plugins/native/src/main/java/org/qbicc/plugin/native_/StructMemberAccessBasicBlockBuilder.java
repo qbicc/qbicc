d8ad9300c8013cb17542e1da3e9f4b439dd69e1b
David M. Lloyd <david.lloyd@redhat.com>
2021-09-28 10:42:58 -0500

    Fix compound type member extraction

--- plugins/native/src/main/java/org/qbicc/plugin/native_/StructMemberAccessBasicBlockBuilder.java
+++ plugins/native/src/main/java/org/qbicc/plugin/native_/StructMemberAccessBasicBlockBuilder.java
@@ -5,8 +5,8 @@ import org.qbicc.graph.BasicBlockBuilder;
 import org.qbicc.graph.DelegatingBasicBlockBuilder;
 import org.qbicc.graph.Value;
 import org.qbicc.graph.ValueHandle;
-import org.qbicc.plugin.layout.Layout;
 import org.qbicc.type.CompoundType;
+import org.qbicc.type.ValueType;
 import org.qbicc.type.definition.element.FieldElement;
 
 public class StructMemberAccessBasicBlockBuilder extends DelegatingBasicBlockBuilder {
@@ -25,9 +25,9 @@ public class StructMemberAccessBasicBlockBuilder extends DelegatingBasicBlockBui
     }
 
     public ValueHandle instanceFieldOf(ValueHandle instance, FieldElement field) {
-        if (instance.getValueType() instanceof CompoundType) {
-            Layout layout = Layout.get(ctxt);
-            return memberOf(instance, layout.getInstanceLayoutInfo(field.getEnclosingType()).getMember(field));
+        ValueType valueType = instance.getValueType();
+        if (valueType instanceof CompoundType) {
+            return memberOf(instance, ((CompoundType) valueType).getMember(field.getName()));
         }
         return super.instanceFieldOf(instance, field);
     }
