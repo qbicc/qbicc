16303a5c17da5aa236447edae13bc9dbc5cddc58
David M. Lloyd <david.lloyd@redhat.com>
2021-09-28 08:46:44 -0500

    Register literal values and offset-of-field values

--- plugins/constants/src/main/java/org/qbicc/plugin/constants/ConstantBasicBlockBuilder.java
+++ plugins/constants/src/main/java/org/qbicc/plugin/constants/ConstantBasicBlockBuilder.java
@@ -4,11 +4,15 @@ import org.qbicc.context.CompilationContext;
 import org.qbicc.graph.BasicBlockBuilder;
 import org.qbicc.graph.DelegatingBasicBlockBuilder;
 import org.qbicc.graph.MemoryAtomicityMode;
+import org.qbicc.graph.Node;
+import org.qbicc.graph.OffsetOfField;
 import org.qbicc.graph.StaticField;
 import org.qbicc.graph.Value;
 import org.qbicc.graph.ValueHandle;
+import org.qbicc.graph.literal.ConstantLiteral;
 import org.qbicc.graph.literal.Literal;
 import org.qbicc.type.definition.element.FieldElement;
+import org.qbicc.type.definition.element.InitializerElement;
 
 /**
  * A basic block builder which substitutes reads from constant static fields with the constant value of the field.
@@ -37,5 +41,20 @@ public class ConstantBasicBlockBuilder extends DelegatingBasicBlockBuilder {
             }
         }
         return getDelegate().load(handle, mode);
+    }
+
+    @Override
+    public Node store(ValueHandle handle, Value value, MemoryAtomicityMode mode) {
+        if (getRootElement() instanceof InitializerElement) {
+            if (handle instanceof StaticField) {
+                final FieldElement fieldElement = ((StaticField) handle).getVariableElement();
+                if (fieldElement.isReallyFinal()) {
+                    if (value instanceof Literal && ! (value instanceof ConstantLiteral) || value instanceof OffsetOfField) {
+                        Constants.get(ctxt).registerConstant(fieldElement, value);
+                    }
+                }
+            }
+        }
+        return super.store(handle, value, mode);
     }
 }
