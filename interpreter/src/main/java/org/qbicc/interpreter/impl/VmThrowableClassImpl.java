9f0f64e93bdce6297c0af9c5021500073bca5de2
David M. Lloyd <david.lloyd@redhat.com>
2021-10-12 08:22:45 -0500

    Support manual construction of throwables with message and cause

--- interpreter/src/main/java/org/qbicc/interpreter/impl/VmThrowableClassImpl.java
+++ interpreter/src/main/java/org/qbicc/interpreter/impl/VmThrowableClassImpl.java
@@ -1,9 +1,15 @@
 package org.qbicc.interpreter.impl;
 
+import java.util.List;
+
+import org.qbicc.context.ClassContext;
 import org.qbicc.interpreter.VmObject;
 import org.qbicc.interpreter.VmThrowable;
 import org.qbicc.interpreter.VmThrowableClass;
 import org.qbicc.type.definition.LoadedTypeDefinition;
+import org.qbicc.type.descriptor.BaseTypeDescriptor;
+import org.qbicc.type.descriptor.ClassTypeDescriptor;
+import org.qbicc.type.descriptor.MethodDescriptor;
 
 /**
  *
@@ -23,18 +29,35 @@ class VmThrowableClassImpl extends VmClassImpl implements VmThrowableClass {
     @Override
     public VmThrowable newInstance(String message) {
         VmThrowableImpl throwable = new VmThrowableImpl(this);
-        getVm().manuallyInitialize(throwable);
+        VmImpl vm = getVm();
+        vm.manuallyInitialize(throwable);
         throwable.initializeDepth();
-        // todo: call ctor
+        LoadedTypeDefinition typeDefinition = getTypeDefinition();
+        ClassContext context = typeDefinition.getContext();
+        ClassTypeDescriptor jls = ClassTypeDescriptor.synthesize(context, "java/lang/String");
+        int ci = typeDefinition.findConstructorIndex(MethodDescriptor.synthesize(context, BaseTypeDescriptor.V, List.of(jls)));
+        if (ci == -1) {
+            throw new IllegalArgumentException("Cannot construct exception with message (no matching ctor) for " + getName());
+        }
+        vm.invokeExact(typeDefinition.getConstructor(ci), throwable, List.of(vm.intern(message)));
         return throwable;
     }
 
     @Override
     public VmThrowable newInstance(String message, VmThrowable cause) {
         VmThrowableImpl throwable = new VmThrowableImpl(this);
-        getVm().manuallyInitialize(throwable);
+        VmImpl vm = getVm();
+        vm.manuallyInitialize(throwable);
         throwable.initializeDepth();
-        // todo: call ctor
+        LoadedTypeDefinition typeDefinition = getTypeDefinition();
+        ClassContext context = typeDefinition.getContext();
+        ClassTypeDescriptor jls = ClassTypeDescriptor.synthesize(context, "java/lang/String");
+        ClassTypeDescriptor jlt = ClassTypeDescriptor.synthesize(context, "java/lang/Throwable");
+        int ci = typeDefinition.findConstructorIndex(MethodDescriptor.synthesize(context, BaseTypeDescriptor.V, List.of(jls, jlt)));
+        if (ci == -1) {
+            throw new IllegalArgumentException("Cannot construct exception with message (no matching ctor) for " + getName());
+        }
+        vm.invokeExact(typeDefinition.getConstructor(ci), throwable, List.of(vm.intern(message), cause));
         return throwable;
     }
 }
