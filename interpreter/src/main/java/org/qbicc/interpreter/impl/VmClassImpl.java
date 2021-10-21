f57bdffb2a260df4747510738deb4844cf924644
David M. Lloyd <david.lloyd@redhat.com>
2021-10-21 15:27:28 -0500

    Prevent exception caused by ExceptionInInitializerError only having one-arg ctor variants

--- interpreter/src/main/java/org/qbicc/interpreter/impl/VmClassImpl.java
+++ interpreter/src/main/java/org/qbicc/interpreter/impl/VmClassImpl.java
@@ -35,8 +35,6 @@ import org.qbicc.type.definition.element.ExecutableElement;
 import org.qbicc.type.definition.element.FieldElement;
 import org.qbicc.type.definition.element.InitializerElement;
 import org.qbicc.type.descriptor.BaseTypeDescriptor;
-import org.qbicc.type.descriptor.ClassTypeDescriptor;
-import org.qbicc.type.descriptor.MethodDescriptor;
 import org.qbicc.type.descriptor.TypeDescriptor;
 
 class VmClassImpl extends VmObjectImpl implements VmClass {
@@ -396,12 +394,12 @@ class VmClassImpl extends VmObjectImpl implements VmClass {
             ClassContext bcc = vm.getCompilationContext().getBootstrapClassContext();
             LoadedTypeDefinition errorType = bcc.findDefinedType("java/lang/ExceptionInInitializerError").load();
             ClassObjectType exType = errorType.getClassType();
-            VmThrowable obj = vm.manuallyInitialize((VmThrowable) vm.allocateObject(exType));
+            VmThrowableClassImpl clazz = (VmThrowableClassImpl) errorType.getVmClass();
+            VmThrowable obj;
             if (initException != null) {
-                MethodDescriptor causeMethodDesc = MethodDescriptor.synthesize(bcc, BaseTypeDescriptor.V, List.of(ClassTypeDescriptor.synthesize(bcc, "java/lang/Throwable")));
-                vm.invokeExact(errorType.resolveConstructorElement(causeMethodDesc), obj, List.of(initException));
+                obj = clazz.newInstance(initException);
             } else {
-                vm.invokeExact(errorType.resolveConstructorElement(MethodDescriptor.VOID_METHOD_DESCRIPTOR), obj, List.of());
+                obj = clazz.newInstance("Failed to initialize " + getName() + " (no nested exception)");
             }
             thread.setThrown(obj);
             throw new Thrown(obj);
