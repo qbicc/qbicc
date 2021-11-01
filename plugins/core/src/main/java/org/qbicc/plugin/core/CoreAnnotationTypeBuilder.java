7c21081961c3043189c45a1b57c7e6fc5bfb7208
David M. Lloyd <david.lloyd@redhat.com>
2021-11-01 07:42:10 -0500

    Add `@NoReflect` and detect annotations on fields and ctors

--- plugins/core/src/main/java/org/qbicc/plugin/core/CoreAnnotationTypeBuilder.java
+++ plugins/core/src/main/java/org/qbicc/plugin/core/CoreAnnotationTypeBuilder.java
@@ -4,9 +4,13 @@ import org.qbicc.context.ClassContext;
 import org.qbicc.type.annotation.Annotation;
 import org.qbicc.type.annotation.AnnotationValue;
 import org.qbicc.type.annotation.EnumConstantAnnotationValue;
+import org.qbicc.type.definition.ConstructorResolver;
 import org.qbicc.type.definition.DefinedTypeDefinition;
+import org.qbicc.type.definition.FieldResolver;
 import org.qbicc.type.definition.MethodResolver;
 import org.qbicc.type.definition.classfile.ClassFile;
+import org.qbicc.type.definition.element.ConstructorElement;
+import org.qbicc.type.definition.element.FieldElement;
 import org.qbicc.type.definition.element.MethodElement;
 import org.qbicc.type.descriptor.ClassTypeDescriptor;
 
@@ -18,6 +22,7 @@ public final class CoreAnnotationTypeBuilder implements DefinedTypeDefinition.Bu
 
     private final ClassTypeDescriptor noSideEffects;
     private final ClassTypeDescriptor hidden;
+    private final ClassTypeDescriptor noReflect;
     private final ClassTypeDescriptor noReturn;
     private final ClassTypeDescriptor noThrow;
     private final ClassTypeDescriptor inline;
@@ -27,6 +32,7 @@ public final class CoreAnnotationTypeBuilder implements DefinedTypeDefinition.Bu
 
         noSideEffects = ClassTypeDescriptor.synthesize(classCtxt, "org/qbicc/runtime/NoSideEffects");
         hidden = ClassTypeDescriptor.synthesize(classCtxt, "org/qbicc/runtime/Hidden");
+        noReflect = ClassTypeDescriptor.synthesize(classCtxt, "org/qbicc/runtime/NoReflect");
         noReturn = ClassTypeDescriptor.synthesize(classCtxt, "org/qbicc/runtime/NoReturn");
         noThrow = ClassTypeDescriptor.synthesize(classCtxt, "org/qbicc/runtime/NoThrow");
         inline = ClassTypeDescriptor.synthesize(classCtxt, "org/qbicc/runtime/Inline");
@@ -50,6 +56,8 @@ public final class CoreAnnotationTypeBuilder implements DefinedTypeDefinition.Bu
                         methodElement.setModifierFlags(ClassFile.I_ACC_HIDDEN);
                     } else if (annotation.getDescriptor().equals(noReturn)) {
                         methodElement.setModifierFlags(ClassFile.I_ACC_NO_RETURN);
+                    } else if (annotation.getDescriptor().equals(noReflect)) {
+                        methodElement.setModifierFlags(ClassFile.I_ACC_NO_REFLECT);
                     } else if (annotation.getDescriptor().equals(noThrow)) {
                         methodElement.setModifierFlags(ClassFile.I_ACC_NO_THROW);
                     } else if (annotation.getDescriptor().equals(inline)) {
@@ -64,6 +72,55 @@ public final class CoreAnnotationTypeBuilder implements DefinedTypeDefinition.Bu
                     }
                 }
                 return methodElement;
+            }
+        }, index);
+    }
+
+    @Override
+    public void addField(FieldResolver resolver, int index) {
+        Delegating.super.addField(new FieldResolver() {
+            @Override
+            public FieldElement resolveField(int index, DefinedTypeDefinition enclosing) {
+                FieldElement fieldElement = resolver.resolveField(index, enclosing);
+                for (Annotation annotation : fieldElement.getInvisibleAnnotations()) {
+                    if (annotation.getDescriptor().equals(noReflect)) {
+                        fieldElement.setModifierFlags(ClassFile.I_ACC_NO_REFLECT);
+                    }
+                }
+                return fieldElement;
+            }
+        }, index);
+    }
+
+    @Override
+    public void addConstructor(ConstructorResolver resolver, int index) {
+        Delegating.super.addConstructor(new ConstructorResolver() {
+            @Override
+            public ConstructorElement resolveConstructor(int index, DefinedTypeDefinition enclosing) {
+                ConstructorElement constructorElement = resolver.resolveConstructor(index, enclosing);
+                for (Annotation annotation : constructorElement.getInvisibleAnnotations()) {
+                    if (annotation.getDescriptor().equals(noSideEffects)) {
+                        constructorElement.setModifierFlags(ClassFile.I_ACC_NO_SIDE_EFFECTS);
+                    } else if (annotation.getDescriptor().equals(hidden)) {
+                        constructorElement.setModifierFlags(ClassFile.I_ACC_HIDDEN);
+                    } else if (annotation.getDescriptor().equals(noReturn)) {
+                        constructorElement.setModifierFlags(ClassFile.I_ACC_NO_RETURN);
+                    } else if (annotation.getDescriptor().equals(noReflect)) {
+                        constructorElement.setModifierFlags(ClassFile.I_ACC_NO_REFLECT);
+                    } else if (annotation.getDescriptor().equals(noThrow)) {
+                        constructorElement.setModifierFlags(ClassFile.I_ACC_NO_THROW);
+                    } else if (annotation.getDescriptor().equals(inline)) {
+                        AnnotationValue value = annotation.getValue("value");
+                        if (value instanceof EnumConstantAnnotationValue ec) {
+                            if (ec.getValueName().equals("ALWAYS")) {
+                                constructorElement.setModifierFlags(ClassFile.I_ACC_ALWAYS_INLINE);
+                            } else if (ec.getValueName().equals("NEVER")) {
+                                constructorElement.setModifierFlags(ClassFile.I_ACC_NEVER_INLINE);
+                            }
+                        }
+                    }
+                }
+                return constructorElement;
             }
         }, index);
     }
