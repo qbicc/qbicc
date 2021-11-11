ebc833ddbf82870762e42a0c8f2ff22004c13d32
David M. Lloyd <david.lloyd@redhat.com>
2021-11-11 17:41:52 -0600

    Test compilers in path preference order first, then service loader order second

--- machine/tool/api/src/main/java/org/qbicc/machine/tool/ToolProvider.java
+++ machine/tool/api/src/main/java/org/qbicc/machine/tool/ToolProvider.java
@@ -28,22 +28,21 @@ public interface ToolProvider {
     static <T extends Tool> Iterable<T> findAllTools(Class<T> type, final Platform platform, Predicate<? super T> filter, ClassLoader classLoader, List<Path> paths) {
         final List<T> list = new ArrayList<>();
         final ServiceLoader<ToolProvider> loader = ServiceLoader.load(ToolProvider.class, classLoader);
-        final Iterator<ToolProvider> iterator = loader.iterator();
-        for (;;)
-            try {
+        for (Path path : paths) {
+            final Iterator<ToolProvider> iterator = loader.iterator();
+            for (;;) try {
                 if (!iterator.hasNext()) {
                     break;
                 }
                 ToolProvider item = iterator.next();
-                for (Path path : paths) {
-                    for (T t : item.findTools(type, platform, path)) {
-                        if (filter.test(t)) {
-                            list.add(t);
-                        }
+                for (T t : item.findTools(type, platform, path)) {
+                    if (filter.test(t)) {
+                        list.add(t);
                     }
                 }
             } catch (ServiceConfigurationError ignored) {
             }
+        }
         return list;
     }
 }
