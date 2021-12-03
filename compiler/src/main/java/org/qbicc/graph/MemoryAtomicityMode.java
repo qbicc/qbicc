2e9e6b667145e91cbe9dfd7a8330c6336896dc07
David M. Lloyd <david.lloyd@redhat.com>
2021-12-03 07:47:24 -0600

    Access modes mapping from old access modes class

--- compiler/src/main/java/org/qbicc/graph/MemoryAtomicityMode.java
+++ compiler/src/main/java/org/qbicc/graph/MemoryAtomicityMode.java
@@ -1,43 +1,56 @@
 package org.qbicc.graph;
 
+import static org.qbicc.graph.atomic.AccessModes.*;
+
+import org.qbicc.graph.atomic.AccessMode;
+import org.qbicc.graph.atomic.AccessModes;
+
 /**
  * The mode for a memory access operation.
+ *
+ * @deprecated Use the modes in {@link AccessModes} instead.
  */
+@Deprecated
 public enum MemoryAtomicityMode {
     /**
      * Non-atomic access (cannot be given for fence or read-modify-write operations).
      */
-    NONE,
+    NONE(SingleUnshared),
     /**
      * Plain access (cannot be given for fence or read-modify-write operations); any value that is read was written by
      * some other operation.
      */
-    UNORDERED,
+    UNORDERED(SinglePlain),
     /**
      * Single total order for a given memory address (cannot be given for fence operations).
      */
-    MONOTONIC,
+    MONOTONIC(SingleOpaque),
     /**
      * Like {@link #MONOTONIC} but also forms a synchronization edge with a corresponding {@link #RELEASE} operation.
      */
-    ACQUIRE,
+    ACQUIRE(SingleAcquire),
     /**
      * Like {@link #MONOTONIC} but also forms a synchronization edge with a corresponding {@link #ACQUIRE} operation.
      */
-    RELEASE,
+    RELEASE(SingleRelease),
     /**
      * Acts as both {@link #ACQUIRE} and {@link #RELEASE} on one operation.
      */
-    ACQUIRE_RELEASE,
+    ACQUIRE_RELEASE(SingleAcqRel),
     /**
      * Like {@link #ACQUIRE_RELEASE} but applying a global total order.
      */
-    SEQUENTIALLY_CONSISTENT,
+    SEQUENTIALLY_CONSISTENT(SingleSeqCst),
     /**
      * Java {@code VOLATILE} access.
      */
-    VOLATILE
-    ;
+    VOLATILE(GlobalSeqCst);
+
+    private final AccessMode accessMode;
+
+    MemoryAtomicityMode(AccessMode accessMode) {
+        this.accessMode = accessMode;
+    }
 
     public static MemoryAtomicityMode max(MemoryAtomicityMode a, MemoryAtomicityMode b) {
         return a.ordinal() > b.ordinal() ? a : b;
@@ -45,6 +58,15 @@ public enum MemoryAtomicityMode {
 
     public static MemoryAtomicityMode min(MemoryAtomicityMode a, MemoryAtomicityMode b) {
         return a.ordinal() < b.ordinal() ? a : b;
+    }
+
+    /**
+     * Get the closest corresponding access mode, which may have to be transformed further before use.
+     *
+     * @return the access mode (not {@code null})
+     */
+    public AccessMode getAccessMode() {
+        return  accessMode;
     }
 
     /**
