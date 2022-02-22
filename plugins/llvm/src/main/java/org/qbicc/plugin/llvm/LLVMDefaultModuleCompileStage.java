12e522ce1f7611af1349225b00469f0ed8c35941
David Grove <groved@us.ibm.com>
2022-02-22 16:29:39 -0500

    add command line option to disable llvm compilation of output files

--- plugins/llvm/src/main/java/org/qbicc/plugin/llvm/LLVMDefaultModuleCompileStage.java
+++ plugins/llvm/src/main/java/org/qbicc/plugin/llvm/LLVMDefaultModuleCompileStage.java
@@ -7,16 +7,20 @@ import java.util.function.Consumer;
 
 public class LLVMDefaultModuleCompileStage implements Consumer<CompilationContext> {
     private final boolean isPie;
+    private final boolean compileOutput;
 
-    public LLVMDefaultModuleCompileStage(boolean isPie) {
+    public LLVMDefaultModuleCompileStage(boolean isPie, boolean compileOutput) {
         this.isPie = isPie;
+        this.compileOutput = compileOutput;
     }
 
     @Override
     public void accept(CompilationContext context) {
         LLVMModuleGenerator generator = new LLVMModuleGenerator(context, isPie ? 2 : 0, isPie ? 2 : 0);
         Path modulePath = generator.processProgramModule(context.getProgramModule(context.getDefaultTypeDefinition()));
-        LLVMCompiler compiler = new LLVMCompiler(context, isPie);
-        compiler.compileModule(context, modulePath);
+        if (compileOutput) {
+            LLVMCompiler compiler = new LLVMCompiler(context, isPie);
+            compiler.compileModule(context, modulePath);
+        }
     }
 }
