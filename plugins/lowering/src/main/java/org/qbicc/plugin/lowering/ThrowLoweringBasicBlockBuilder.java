f090662605956a901012d781b00bfa3fd8f92223
David M. Lloyd <david.lloyd@redhat.com>
2022-03-01 10:58:58 -0600

    Fix throw lowering

--- plugins/lowering/src/main/java/org/qbicc/plugin/lowering/ThrowLoweringBasicBlockBuilder.java
+++ plugins/lowering/src/main/java/org/qbicc/plugin/lowering/ThrowLoweringBasicBlockBuilder.java
@@ -7,6 +7,10 @@ import org.qbicc.graph.BasicBlock;
 import org.qbicc.graph.BasicBlockBuilder;
 import org.qbicc.graph.DelegatingBasicBlockBuilder;
 import org.qbicc.graph.Value;
+import org.qbicc.object.FunctionDeclaration;
+import org.qbicc.type.FunctionType;
+import org.qbicc.type.MethodType;
+import org.qbicc.type.TypeSystem;
 import org.qbicc.type.definition.element.FieldElement;
 
 import static org.qbicc.graph.atomic.AccessModes.SingleUnshared;
@@ -20,13 +24,18 @@ public class ThrowLoweringBasicBlockBuilder extends DelegatingBasicBlockBuilder 
     }
 
     public BasicBlock throw_(final Value value) {
-        BasicBlockBuilder fb = getFirstBuilder();
+        TypeSystem ts = ctxt.getTypeSystem();
         ThrowExceptionHelper teh = ThrowExceptionHelper.get(ctxt);
         FieldElement exceptionField = ctxt.getExceptionField();
-        fb.store(fb.instanceFieldOf(fb.referenceHandle(fb.load(fb.currentThread(), SingleUnshared)), exceptionField), value, SingleUnshared);
+        store(instanceFieldOf(referenceHandle(load(currentThread(), SingleUnshared)), exceptionField), value, SingleUnshared);
 
         // TODO Is this safe? Can the java/lang/Thread object be moved while this pointer is still in use?
-        Value ptr = fb.bitCast(fb.addressOf(fb.instanceFieldOf(fb.referenceHandle(fb.load(fb.currentThread(), SingleUnshared)), teh.getUnwindExceptionField())), teh.getUnwindExceptionField().getType().getPointer());
-        return fb.callNoReturn(fb.staticMethod(teh.getRaiseExceptionMethod()), List.of(ptr));
+        Value ptr = bitCast(addressOf(instanceFieldOf(referenceHandle(load(currentThread(), SingleUnshared)), teh.getUnwindExceptionField())), teh.getUnwindExceptionField().getType().getPointer());
+
+        String functionName = "_Unwind_RaiseException";
+        MethodType origType = teh.getRaiseExceptionMethod().getType();
+        FunctionType functionType = ts.getFunctionType(origType.getReturnType(), origType.getParameterTypes());
+        FunctionDeclaration decl = ctxt.getImplicitSection(getRootElement()).declareFunction(teh.getRaiseExceptionMethod(), functionName, functionType);
+        return callNoReturn(pointerHandle(ctxt.getLiteralFactory().literalOf(decl)), List.of(ptr));
     }
 }
