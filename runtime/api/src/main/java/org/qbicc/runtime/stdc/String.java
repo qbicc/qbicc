4496228628de6ddc2f613f9af0a43ad7c946fc35
David M. Lloyd <david.lloyd@redhat.com>
2022-03-03 10:09:17 -0600

    Add `memmove` to stdc defs

--- runtime/api/src/main/java/org/qbicc/runtime/stdc/String.java
+++ runtime/api/src/main/java/org/qbicc/runtime/stdc/String.java
@@ -14,6 +14,7 @@ public class String {
     public static native char_ptr strerror(c_int errNum);
 
     public static native void_ptr memcpy(void_ptr dest, const_void_ptr src, size_t n);
+    public static native void_ptr memmove(void_ptr dest, const_void_ptr src, size_t n);
 
     public static native void_ptr memset(void_ptr dest, c_int data, size_t len);
 
