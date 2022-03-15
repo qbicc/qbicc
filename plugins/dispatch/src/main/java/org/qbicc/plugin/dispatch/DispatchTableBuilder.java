fc19527d7e7c01dd6ea2477bbb54f4b7096e3afc
David Grove <groved@us.ibm.com>
2022-03-14 22:15:06 -0400

    fix vtable layout for VarHandle subclasses

--- plugins/dispatch/src/main/java/org/qbicc/plugin/dispatch/DispatchTableBuilder.java
+++ plugins/dispatch/src/main/java/org/qbicc/plugin/dispatch/DispatchTableBuilder.java
@@ -15,12 +15,17 @@ public class DispatchTableBuilder implements Consumer<CompilationContext>  {
         DispatchTables tables = DispatchTables.get(ctxt);
 
         // Starting from java.lang.Object walk down the live class hierarchy and
-        //  compute vtable layouts that contain just the methods where ctxt.wasEnqueued is true.
+        //  compute vtable layouts that contain just the dispatchable methods.
         ClassContext classContext = ctxt.getBootstrapClassContext();
         DefinedTypeDefinition jloDef = classContext.findDefinedType("java/lang/Object");
         LoadedTypeDefinition jlo = jloDef.load();
         tables.buildFilteredVTable(jlo);
         info.visitReachableSubclassesPreOrder(jlo, tables::buildFilteredVTable);
+
+        // Now adjust the vtables of VarHandle subclasses to account for our implementation trick of "overriding" SigPoly methods
+        LoadedTypeDefinition vh = classContext.findDefinedType("java/lang/invoke/VarHandle").load();
+        DispatchTables.VTableInfo vhInfo = tables.getVTableInfo(vh);
+        info.visitReachableSubclassesPreOrder(vh, c -> tables.adjustVTableForSigPloySubclass(c, vhInfo));
 
         // Synthesize GlobalVariable for vtables[] and itable_dict[]
         tables.buildVTablesGlobal(jlo);
@@ -29,7 +34,7 @@ public class DispatchTableBuilder implements Consumer<CompilationContext>  {
         // Synthesize GlobalVariable for rtinit[]
         tables.buildRTInitGlobal(jlo);
 
-        // Now build the interface dispatching structures for the reachable methods
+        // Now build the interface dispatching structures for the dispatchable methods
         info.visitReachableInterfaces(tables::buildFilteredITableForInterface);
     }
 }
