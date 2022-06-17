940cd1772446c190356b8f9eeee2b8e5e538b591
David M. Lloyd <david.lloyd@redhat.com>
2022-06-17 10:09:42 -0500

    Update integration tests accordingly

--- integration-tests/src/it/java/org/qbicc/tests/integration/utils/Qbicc.java
+++ integration-tests/src/it/java/org/qbicc/tests/integration/utils/Qbicc.java
@@ -10,7 +10,7 @@ import org.qbicc.main.Main;
 
 public class Qbicc {
     public static DiagnosticContext build(Path outputPath, Path nativeOutputPath, String mainClass, Logger logger) {
-        return Main.builder().appendBootPath(ClassPathEntry.of(outputPath))
+        return Main.builder().addAppPath(ClassPathEntry.of(outputPath))
             .setOutputPath(nativeOutputPath)
             .setDiagnosticsHandler(new QbiccDiagnosticLogger(logger))
             .setGraphGenConfig(new GraphGenConfig())
