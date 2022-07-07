125e192326d2192a2cacad0a0e6b270af66bf790
David M. Lloyd <david.lloyd@redhat.com>
2022-07-07 11:08:00 -0500

    Enable phase 3

--- interpreter/src/main/java/org/qbicc/interpreter/impl/VmImpl.java
+++ interpreter/src/main/java/org/qbicc/interpreter/impl/VmImpl.java
@@ -998,8 +998,7 @@ public final class VmImpl implements Vm {
         // phase 2
         invokeExact(systemType.getMethod(systemType.findSingleMethodIndex(me -> me.nameEquals("initPhase2"))), null, List.of(Boolean.FALSE, Boolean.FALSE));
         // phase 3
-        // TODO: Haven't tried yet...still working on phase2
-        // invokeExact(systemType.getMethod(systemType.findSingleMethodIndex(me -> me.nameEquals("initPhase3"))), null, List.of());
+        invokeExact(systemType.getMethod(systemType.findSingleMethodIndex(me -> me.nameEquals("initPhase3"))), null, List.of());
 
         // Initialize early to avoid deadlocks
         initialize(bootstrapClassLoader.loadClass("java/lang/ref/Reference"));
