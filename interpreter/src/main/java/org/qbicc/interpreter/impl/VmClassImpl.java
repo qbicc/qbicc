e9ddcd4ee675966e64235892c93c0363bb71d938
David Grove <groved@us.ibm.com>
2022-11-04 11:59:32 -0400

    bug fix: store the right Class's modifiers in the VmClass object!
    We were storing the modifiers for java.lang.Class in every VmClass
    object instead of the Class's actual modifiers!

--- interpreter/src/main/java/org/qbicc/interpreter/impl/VmClassImpl.java
+++ interpreter/src/main/java/org/qbicc/interpreter/impl/VmClassImpl.java
@@ -373,7 +373,7 @@ class VmClassImpl extends VmObjectImpl implements VmClass {
                 memory.store32(getVmClass().getLayoutInfo().getMember(coreClasses.getClassInstanceSizeField()).getOffset(), layoutInfo.getCompoundType().getSize(), SinglePlain);
                 memory.store8(getVmClass().getLayoutInfo().getMember(coreClasses.getClassInstanceAlignField()).getOffset(), layoutInfo.getCompoundType().getAlign(), SinglePlain);
             }
-            memory.store32(getVmClass().getLayoutInfo().getMember(classDef.findField("modifiers")).getOffset(), classDef.getModifiers(), SinglePlain);
+            memory.store32(getVmClass().getLayoutInfo().getMember(classDef.findField("modifiers")).getOffset(), typeDefinition.getModifiers(), SinglePlain);
             setPointerField(classDef, "referenceBitMap", computeBitMap());
         } catch (Exception e) {
             // for breakpoints
