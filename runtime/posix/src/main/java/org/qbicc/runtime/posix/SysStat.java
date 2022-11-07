6b456dc3012140d924e4312358c605953b4be0b9
David Grove <groved@us.ibm.com>
2022-11-07 15:28:55 -0500

    add native method for mkdir to SysStat

--- runtime/posix/src/main/java/org/qbicc/runtime/posix/SysStat.java
+++ runtime/posix/src/main/java/org/qbicc/runtime/posix/SysStat.java
@@ -81,4 +81,6 @@ public final class SysStat {
     public static native c_int lstat(const_char_ptr pathName, struct_stat_ptr statBuf);
     @name(value = "fstatat$INODE64", when = { Build.Target.IsMacOs.class, Build.Target.IsAmd64.class })
     public static native c_int fstatat(c_int dirFd, const_char_ptr pathName, struct_stat_ptr statBuf, c_int flags);
+
+    public static native c_int mkdir(const_char_ptr pathname, mode_t mode);
 }
