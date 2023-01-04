6b380c28ec7380ca8a64439ce5d4583ac7cda3f8
David Grove <groved@us.ibm.com>
2023-01-04 17:22:29 -0500

    ImageInfo fixes
    1. Quarkus also expects inImageRuntimeCore
    2. Simplify implementation by using Build.isHost/isTarget

--- runtime/api/src/main/java/org/graalvm/nativeimage/ImageInfo.java
+++ runtime/api/src/main/java/org/graalvm/nativeimage/ImageInfo.java
@@ -1,8 +1,15 @@
 package org.graalvm.nativeimage;
 
+import org.qbicc.runtime.Build;
+
 // TODO: works around direct reference to org.graalvm.nativeimage.ImageInfo in quarkus 2.14
 //       Once we upgrade quarkus-qbicc to a Quarkus version that includes
 //       https://github.com/quarkusio/quarkus/pull/29993 we can get rid of this Hook.
 public class ImageInfo {
-    public static native boolean inImageBuildtimeCode();
+    public static boolean inImageBuildtimeCode() {
+        return Build.isHost();
+    }
+    public static boolean inImageRuntimeCode() {
+        return Build.isTarget();
+    }
 }
