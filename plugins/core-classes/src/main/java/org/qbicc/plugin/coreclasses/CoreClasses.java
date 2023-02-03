014c535915a5a344fe7b026f412271444d01c393
David Grove <groved@us.ibm.com>
2023-02-03 17:11:02 -0500

    typeId and header fields now defined normally in classlib

--- plugins/core-classes/src/main/java/org/qbicc/plugin/coreclasses/CoreClasses.java
+++ plugins/core-classes/src/main/java/org/qbicc/plugin/coreclasses/CoreClasses.java
@@ -279,30 +279,6 @@ public final class CoreClasses {
         Patcher patcher = Patcher.get(ctxt);
         ClassContext classContext = ctxt.getBootstrapClassContext();
 
-        // inject a field to hold the object header bits
-        patcher.addField(classContext, OBJECT_INT_NAME, "header", BaseTypeDescriptor.V, new FieldResolver() {
-            @Override
-            public FieldElement resolveField(int index, DefinedTypeDefinition enclosing, FieldElement.Builder builder) {
-                builder.setModifiers(ClassFile.ACC_PRIVATE | ClassFile.ACC_VOLATILE | ClassFile.I_ACC_NO_REFLECT | ClassFile.I_ACC_NO_RESOLVE);
-                builder.setEnclosingType(enclosing);
-                builder.setSignature(BaseTypeSignature.V);
-                builder.setType(HeaderBits.get(ctxt).getHeaderType());
-                return builder.build();
-            }
-        }, 0, 0);
-
-        // inject a field to hold the object typeId
-        patcher.addField(classContext, OBJECT_INT_NAME, "typeId", BaseTypeDescriptor.V, new FieldResolver() {
-            @Override
-            public FieldElement resolveField(int index, DefinedTypeDefinition enclosing, FieldElement.Builder builder) {
-                builder.setModifiers(ClassFile.ACC_PRIVATE | ClassFile.ACC_FINAL | ClassFile.I_ACC_NO_REFLECT | ClassFile.I_ACC_NO_RESOLVE);
-                builder.setEnclosingType(enclosing);
-                builder.setSignature(BaseTypeSignature.V);
-                builder.setTypeResolver(e -> e.getEnclosingType().load().getClassType().getTypeType());
-                return builder.build();
-            }
-        }, 0, 0);
-
         // now inject a field of ClassObjectType into Class to hold the corresponding run time type
         patcher.addField(classContext, CLASS_INT_NAME, "id", BaseTypeDescriptor.V, new FieldResolver() {
             @Override
