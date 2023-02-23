a5e5436bd7754bb6f3f8601cce10882c13e16f94
David M. Lloyd <david.lloyd@redhat.com>
2023-02-23 09:46:06 -0600

    Disallow generic functional interfaces

--- plugins/native/src/main/java/org/qbicc/plugin/native_/FunctionTypeResolver.java
+++ plugins/native/src/main/java/org/qbicc/plugin/native_/FunctionTypeResolver.java
@@ -48,8 +48,7 @@ public class FunctionTypeResolver implements DescriptorTypeResolver.Delegating {
                     FunctionType functionType;
                     // check the signature
                     TypeSystem ts = ctxt.getTypeSystem();
-                    if (signature instanceof ClassTypeSignature) {
-                        ClassTypeSignature sig = (ClassTypeSignature) signature;
+                    if (signature instanceof ClassTypeSignature sig) {
                         if (! sig.getIdentifier().equals(Native.FUNCTION)) {
                             ctxt.warning("Incorrect generic signature (expected a %s but got \"%s\")", function.class, sig);
                             break out;
@@ -62,58 +61,53 @@ public class FunctionTypeResolver implements DescriptorTypeResolver.Delegating {
                         TypeArgument typeArgument = args.get(0);
                         if (typeArgument instanceof AnyTypeArgument) {
                             ctxt.error("Function types cannot be open-ended");
-                            functionType = ts.getFunctionType(ts.getVoidType());
+                            return ts.getFunctionType(ts.getVoidType(), List.of());
                         } else {
                             assert typeArgument instanceof BoundTypeArgument;
                             BoundTypeArgument bound = (BoundTypeArgument) typeArgument;
                             if (bound.getVariance() != Variance.INVARIANT) {
                                 ctxt.error("Function types cannot be open-ended");
+                                return ts.getFunctionType(ts.getVoidType(), List.of());
                             }
                             // looks right, get the proper pointee type
                             ReferenceTypeSignature pointeeSig = bound.getBound();
-                            // todo: use context to resolve type variable bounds
-                            TypeDescriptor pointeeDesc = pointeeSig.asDescriptor(classCtxt);
-                            if (pointeeDesc instanceof ClassTypeDescriptor) {
-                                ClassTypeDescriptor classDesc = (ClassTypeDescriptor) pointeeDesc;
-                                final String name;
-                                if (classDesc.getPackageName().isEmpty()) {
-                                    name = classDesc.getClassName();
-                                } else {
-                                    name = classDesc.getPackageName() + '/' + classDesc.getClassName();
-                                }
-                                if (name.equals("java/lang/Object")) {
-                                    // special case: it's really an "any" pointer with extra front-end guards on it
-                                    return ts.getVoidType().getPointer();
-                                }
-                                DefinedTypeDefinition definedType = classCtxt.findDefinedType(name);
-                                if (definedType == null) {
-                                    ctxt.error("Function interface type \"%s\" is missing", name);
-                                    functionType = ts.getFunctionType(ts.getVoidType());
-                                } else {
-                                    if (definedType.isInterface()) {
-                                        List<TypeArgument> functionalInterfaceArguments = ((ClassTypeSignature)bound.getBound()).getTypeArguments();
-                                        return NativeInfo.get(ctxt).getTypeOfFunctionalInterface(definedType, functionalInterfaceArguments);
-                                    } else {
-                                        ctxt.error("Function interface type \"%s\" is not an interface", name);
-                                        functionType = ts.getFunctionType(ts.getVoidType());
-                                    }
-                                }
-                            } else {
+                            if (! (pointeeSig instanceof ClassTypeSignature ctsBound)) {
                                 ctxt.error("Function pointee must be a functional interface");
-                                functionType = ts.getFunctionType(ts.getVoidType());
+                                return ts.getFunctionType(ts.getVoidType(), List.of());
+                            }
+                            // todo: use context to resolve type variable bounds
+                            ClassTypeDescriptor classDesc = ctsBound.asDescriptor(classCtxt);
+                            final String name;
+                            if (classDesc.getPackageName().isEmpty()) {
+                                name = classDesc.getClassName();
+                            } else {
+                                name = classDesc.getPackageName() + '/' + classDesc.getClassName();
+                            }
+                            if (name.equals("java/lang/Object")) {
+                                // special case: it's really an "any" pointer with extra front-end guards on it
+                                ctxt.error("Function types cannot be open-ended");
+                                return ts.getFunctionType(ts.getVoidType(), List.of());
+                            }
+                            DefinedTypeDefinition definedType = classCtxt.findDefinedType(name);
+                            if (definedType == null) {
+                                ctxt.error("Function interface type \"%s\" is missing", name);
+                                return ts.getFunctionType(ts.getVoidType(), List.of());
+                            } else {
+                                if (definedType.isInterface()) {
+                                    return NativeInfo.get(ctxt).getTypeOfFunctionalInterface(definedType, ctsBound);
+                                } else {
+                                    ctxt.error("Function interface type \"%s\" is not an interface", name);
+                                    return ts.getFunctionType(ts.getVoidType(), List.of());
+                                }
                             }
                         }
                     } else {
                         // todo: how can we cleanly get the location from here?
                         ctxt.warning("Generic signature type mismatch (expected a %s but got a %s)", ClassTypeSignature.class, signature.getClass());
-                        functionType = ts.getFunctionType(ts.getVoidType());
+                        functionType = ts.getFunctionType(ts.getVoidType(), List.of());
                     }
                     return functionType;
-                } else {
-                    break out;
                 }
-            } else {
-                break out;
             }
         }
         return getDelegate().resolveTypeFromDescriptor(descriptor, paramCtxt, signature);
